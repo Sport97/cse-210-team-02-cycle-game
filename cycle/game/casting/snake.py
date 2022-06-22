8e64578bab305ec3777944f22ce2ad3c8d66f010
mohamed Nasiru <mednasiru24@gmail.com>
2022-06-22 12:24:54 +0000

    updates after zoom

--- cycle/game/casting/snake.py
+++ cycle/game/casting/snake.py
@@ -15,15 +15,26 @@ class Snake(Actor):
     def __init__(self):
         super().__init__()
         self._segments = []
+        self._second_segments = []
         self._prepare_body()
+        self._prepare_second_snake_body()
+        self.color_head()
+
 
     def get_segments(self):
         return self._segments
+
+    def get_second_snake_segments(self):
+        return self._second_segments
+
 
     def move_next(self):
         # move all segments
         for segment in self._segments:
             segment.move_next()
+        for second_segment in self._second_segments:
+            second_segment.move_next()
+
         # update velocities
         for i in range(len(self._segments) - 1, 0, -1):
             trailing = self._segments[i]
@@ -31,8 +42,18 @@ class Snake(Actor):
             velocity = previous.get_velocity()
             trailing.set_velocity(velocity)
 
+        for i in range(len(self._second_segments) - 1, 0, -1):
+            trailing = self._second_segments[i]
+            previous = self._second_segments[i - 1]
+            velocity = previous.get_velocity()
+            trailing.set_velocity(velocity)
+
     def get_head(self):
+        
         return self._segments[0]
+    
+    def get_second_snake_head(self):
+        return self._second_segments[0]
 
     def grow_tail(self, number_of_segments):
         for i in range(number_of_segments):
@@ -47,19 +68,43 @@ class Snake(Actor):
             segment.set_text("#")
             segment.set_color(constants.GREEN)
             self._segments.append(segment)
+        
+
+    def grow_second_snake_tail(self, number_of_segments):
+        for i in range(number_of_segments):
+            tail = self._second_segments[-1]
+            velocity = tail.get_velocity()
+            offset = velocity.reverse()
+            position = tail.get_position().add(offset)
+            
+            
+            second_segment = Actor()
+            second_segment.set_position(position)
+            second_segment.set_velocity(velocity)
+            second_segment.set_text("0")
+            second_segment.set_color(constants.YELLOW)
+            self._second_segments.append(second_segment)
+
 
     def turn_head(self, velocity):
         self._segments[0].set_velocity(velocity)
+        self._second_segments[0].set_velocity(velocity)
+
+    def color_head(self):
+         self._segments[0].set_color(constants.RED)
+         self._second_segments[0].set_color(constants.RED)
+       
     
     def _prepare_body(self):
-        x = int(constants.MAX_X / 2)
-        y = int(constants.MAX_Y / 2)
+        x = int(constants.MAX_X/2)
+        y = int(constants.MAX_Y/4 )
+        
 
         for i in range(constants.SNAKE_LENGTH):
             position = Point(x - i * constants.CELL_SIZE, y)
             velocity = Point(1 * constants.CELL_SIZE, 0)
             text = "8" if i == 0 else "#"
-            color = constants.YELLOW if i == 0 else constants.GREEN
+            color = constants.GREEN
             
             segment = Actor()
             segment.set_position(position)
@@ -67,3 +112,19 @@ class Snake(Actor):
             segment.set_text(text)
             segment.set_color(color)
             self._segments.append(segment)
+
+    def _prepare_second_snake_body(self):
+       x = int(constants.MAX_X/2)
+       y = int(constants.MAX_Y /2)
+
+       for i in range(constants.SNAKE_LENGTH):
+            position = Point(x - i * constants.CELL_SIZE, y)
+            velocity = Point(1 * constants.CELL_SIZE, 0)
+            text = "8" if i == 0 else "0"
+            color =  constants.YELLOW
+            segment = Actor()
+            segment.set_position(position)
+            segment.set_velocity(velocity)
+            segment.set_text(text)
+            segment.set_color(color)
+            self._second_segments.append(segment)
--- cycle/game/scripting/draw_actors_action.py
+++ cycle/game/scripting/draw_actors_action.py
@@ -19,25 +19,22 @@ class DrawActorsAction(Action):
         """
         self._video_service = video_service
 
-    def execute(self, cast, script):
+    def execute(self, cast, script): 
         """Executes the draw actors action.
 
         Args:
             cast (Cast): The cast of Actors in the game.
             script (Script): The script of Actions in the game.
         """
-        score = cast.get_first_actor("scores")
-        food = cast.get_first_actor("foods")
+
         snake = cast.get_first_actor("snakes")
         second_snake = cast.get_first_actor("second snake")
         segments = snake.get_segments()
-        second_segments = second_snake.get_segments()
+        second_segments = second_snake.get_second_snake_segments()
         messages = cast.get_actors("messages")
 
         self._video_service.clear_buffer()
-        self._video_service.draw_actor(food)
         self._video_service.draw_actors(segments)
         self._video_service.draw_actors(second_segments)
-        self._video_service.draw_actor(score)
         self._video_service.draw_actors(messages, True)
         self._video_service.flush_buffer()
