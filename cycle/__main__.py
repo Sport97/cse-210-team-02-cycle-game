d3ea23ea64898715010a470aae66eb05aee12f80
MadalynM <mou21015@byui.edu>
2022-06-22 23:05:11 -0600

    Cleaned up Code

--- cycle/__main__.py
+++ cycle/__main__.py
@@ -1,7 +1,5 @@
-import constants
 
 from game.casting.cast import Cast
-from game.casting.food import Food
 from game.casting.score import Score
 from game.casting.snake import Snake
 from game.scripting.script import Script
@@ -12,15 +10,12 @@ from game.scripting.draw_actors_action import DrawActorsAction
 from game.directing.director import Director
 from game.services.keyboard_service import KeyboardService
 from game.services.video_service import VideoService
-from game.shared.color import Color
-from game.shared.point import Point
 
 
 def main():
     
     # create the cast
     cast = Cast()
-    cast.add_actor("foods", Food())
     cast.add_actor("snakes", Snake())
     cast.add_actor("second snake", Snake())
     cast.add_actor("scores", Score())
